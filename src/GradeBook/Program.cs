1714927261b6ce058308b8d43cbac26785e77045
Devin Udy <devin.udy@gmail.com>
2020-04-23 12:40:48 -0600

    Looped an array of numbers to find the sum

--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -6,6 +6,13 @@ namespace GradeBook
     {
         static void Main(string[] args)
         {
+            var numbers = new[] { 12.7, 3.14, 420, 911 };
+            var result = 0.0;
+            foreach (var number in numbers)
+            {
+                result += number;
+            }
+            Console.WriteLine(result);
             if (args.Length > 0)
             {
                 Console.WriteLine($"Hello, {args[0]}!");
