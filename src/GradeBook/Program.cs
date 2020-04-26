23a05d4829b99a1ad8d1b6db3baf6eceec83f05a
Devin Udy <devin.udy@gmail.com>
2020-04-26 11:42:01 -0600

    Created another file and method

--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -7,7 +7,9 @@ namespace GradeBook
     {
         static void Main(string[] args)
         {
-            var numbers = new[] { 12.7, 3.14, 420, 911 };
+            var book = new Book();
+            book.AddGrade(89.1);
+
             var grades = new List<double>() { 12.7, 10.3, 6.11, 4.1 };
             grades.Add(56.1);
 
@@ -16,17 +18,9 @@ namespace GradeBook
             {
                 result += number;
             }
-            Console.WriteLine(result);
+            Console.WriteLine($"Sum of Grades: {result}");
             result /= grades.Count;
             Console.WriteLine($"Average Grade: {result:N1}"); //Use N# to specify and round how many digits after the decimal
-            if (args.Length > 0)
-            {
-                Console.WriteLine($"Hello, {args[0]}!");
-            }
-            else
-            {
-                Console.WriteLine("Hello!");
-            }
         }
     }
 }
