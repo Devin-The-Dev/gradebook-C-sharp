b6cae741cfbb7eae0bfa0714ddda05929c69d601
Devin Udy <devin.udy@gmail.com>
2020-04-25 20:17:06 -0600

    Found avarage of grades variable

--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace GradeBook
 {
@@ -7,12 +8,17 @@ namespace GradeBook
         static void Main(string[] args)
         {
             var numbers = new[] { 12.7, 3.14, 420, 911 };
+            var grades = new List<double>() { 12.7, 10.3, 6.11, 4.1 };
+            grades.Add(56.1);
+
             var result = 0.0;
-            foreach (var number in numbers)
+            foreach (var number in grades)
             {
                 result += number;
             }
             Console.WriteLine(result);
+            result /= grades.Count;
+            Console.WriteLine($"Average Grade: {result:N1}"); //Use N# to specify and round how many digits after the decimal
             if (args.Length > 0)
             {
                 Console.WriteLine($"Hello, {args[0]}!");
